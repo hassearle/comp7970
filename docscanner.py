d72124b1dc467f1171d994240ce44eeedadf0b2a
Unknown <tms0050@auburn.edu>
2017-11-20 16:54:31 -0600

    Bayes Fixed
    Biggest probability class stored in new matrix. Fixed algorithm to professor specification

--- docscanner.py
+++ docscanner.py
@@ -6,7 +6,7 @@
 
 dictionary = {}
 prevVal = ""
-with open("/root/code/comp7970/Dataset/CA-GrQc.txt") as f:          # opens file
+with open("C:\Users\Tyler\Documents\School 17-18\Big Data 17\Dataset\CA-GrQc2.txt") as f:          # opens file
     totalAuthors = 5242
     j = 0
     for line in f:                                                  # loops thru db
@@ -29,7 +29,7 @@ with open("/root/code/comp7970/Dataset/CA-GrQc.txt") as f:          # opens file
 # BINS 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-with open("/root/code/comp7970/Dataset/CA-GrQc.txt") as g:          # opens file
+with open("C:\Users\Tyler\Documents\School 17-18\Big Data 17\Dataset\CA-GrQc2.txt") as g:          # opens file
     
     binAmt = 10                                                     # number of bins to partition db
     totalRange = big - small                                        # db range
@@ -43,7 +43,7 @@ with open("/root/code/comp7970/Dataset/CA-GrQc.txt") as g:          # opens file
                                                                                     # ie:
                                                                                     # row 1: binA = 1, binB = 4,...
                                                                                     # row 2: binA = 1+2, binB = 4+1,...
-    binRowSum = [0 for x in xrange(totalAuthors)]                                                               # sum of bins 1-10 for each row        
+    binRowSum = [0 for x in xrange(totalAuthors)]                                                               # sum of bins 1-10 for each row
     for i in range(0, totalAuthors):
         for j in range(0, binAmt):
             intMatrix[i][j] = 0 
@@ -66,7 +66,7 @@ with open("/root/code/comp7970/Dataset/CA-GrQc.txt") as g:          # opens file
         
         prevVal = val
 
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # SPLIT DATASET
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # split dataset into training/test data
@@ -74,12 +74,15 @@ with open("/root/code/comp7970/Dataset/CA-GrQc.txt") as g:          # opens file
 splitRatio = .3
 splitTotal = splitRatio * totalAuthors
 trainingColMatrix = [0 for x in xrange(binAmt)]
+trainingMatTotal = 0
 for a in range(0, int(splitTotal)):
     for b in range(0, binAmt):
         trainingColMatrix[b] += intMatrix[a][b]
 print trainingColMatrix
-del a
-del b
+for c in range(0, binAmt):
+    trainingMatTotal += trainingColMatrix[c]
+print trainingMatTotal
+del a, b, c, f, g, i, j, line, other, trash, x, y, big, small, coauthVal, currentBin, dbRow, prevVal, prevBin, splitRatio
 
 
 
@@ -90,16 +93,24 @@ del b
 
 biggestProb = 0.0
 biggestProbCol = 0
+fldivision = 0.0
+biggestProbVis = [0 for x in xrange(0, 5242)]
 for a in range(int(splitTotal), 5242):
     for b in range(0, 10):
-        denominator = binRowSum[a] * trainingColMatrix[b]
-        numerator = 0.0 + intMatrix[a][b]
-        if (numerator / denominator > biggestProb):
-            biggestProb = numerator / denominator
-            biggestProbCol = b
-    print dictionary[a]
-    print intMatrix[a]
-    print biggestProbCol + 1
-    print trainingColMatrix
+        denominator = binRowSum[a] * trainingMatTotal
+        numerator = trainingColMatrix[b] * intMatrix[a][b]
+        fldivision = float(numerator)/float(denominator)
+        if (fldivision > biggestProb):
+            biggestProb = fldivision
+            biggestProbCol = b + 1
+    biggestProbVis[a] = biggestProbCol
+    #print dictionary[a]
+    #print intMatrix[a]
+    #print biggestProbCol
+    #print trainingColMatrix
     biggestProb = 0.0
     # IMPLEMENT BAYES
+for x in range(0, 5242):
+    print intMatrix[x]
+    print binRowSum[x]
+    print biggestProbVis[x]
