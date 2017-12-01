716d3c7eefa175a99d62ef8c31b5bd157f0d6b43
Unknown <tms0050@auburn.edu>
2017-11-30 20:32:26 -0600

    Bootstrap Algorithm moved to Undirected Bayes

--- UndirectedBayesImplementation.py
+++ UndirectedBayesImplementation.py
@@ -16,7 +16,7 @@ with open("Dataset\com-dblp.ungraph.txt") as f:
                 biggest = int(val)
             if(int(val2) > biggest):
                 biggest = int(val2)
-intMatrix = [[0 for x in xrange(binAmt + 1)] for y in xrange(biggest + 1)]
+tupleAdjacencyMatrix = [[0 for x in xrange(binAmt + 1)] for y in xrange(biggest + 1)]
 binwidth = biggest/binAmt + 1
 with open("Dataset\com-dblp.ungraph.txt") as g:
     lineNum = 1
@@ -33,19 +33,19 @@ with open("Dataset\com-dblp.ungraph.txt") as g:
                 firstBin -= 1
             if(secondBin == binAmt):
                 secondBin -= 1
-            intMatrix[firstPos][secondBin] += 1
-            intMatrix[firstPos][10] += 1
-            intMatrix[secondPos][firstBin] += 1
-            intMatrix[secondPos][10] += 1
+            tupleAdjacencyMatrix[firstPos][secondBin] += 1
+            tupleAdjacencyMatrix[firstPos][10] += 1
+            tupleAdjacencyMatrix[secondPos][firstBin] += 1
+            tupleAdjacencyMatrix[secondPos][10] += 1
 dictionary = {}
 j = 0
 for a in xrange(biggest):
-    if(intMatrix[a][10] > 0):
+    if(tupleAdjacencyMatrix[a][10] > 0):
         dictionary[j] = a
         j += 1
 for a in xrange(biggest, 0, -1):
-    if(intMatrix[a][10] == 0):
-        del intMatrix[a]
+    if(tupleAdjacencyMatrix[a][10] == 0):
+        del tupleAdjacencyMatrix[a]
 
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # IMPLEMENT BAYES
@@ -53,36 +53,94 @@ for a in xrange(biggest, 0, -1):
     # Use Bayes Algorithm on the data not in training set and also set up evaluation data
 
 import random
-def implement_bayes(coeff):
+def implement_holdout_bayes(coeff):
     testDataMat = [[0 for x in xrange(binAmt + 1)] for y in xrange(totalAuthors)]
+    trainingDataMat = [[0 for x in xrange(binAmt + 1)] for y in xrange(totalAuthors)]
     colSum = [0 for x in xrange(binAmt)]
     maxRow = 0
+    tMaxRow = 0
     tDataEdges = 0
     for a in range(totalAuthors):
         checker = random.uniform(0,1)
         if(checker > coeff):
-            for b in xrange(10):
-                testDataMat[maxRow][b] = intMatrix[a][b]
+            for b in xrange(binAmt):
+                testDataMat[maxRow][b] = tupleAdjacencyMatrix[a][b]
             testDataMat[maxRow][10] = a
             maxRow += 1
         else:
-            for b in xrange(10):
-                colSum[b] += intMatrix[a][b]
-                tDataEdges += intMatrix[a][b]
-    biggestProb = 0.0
+            for b in xrange(binAmt):
+                trainingDataMat[tMaxRow][b] += tupleAdjacencyMatrix[a][b]
+                colSum[b] += tupleAdjacencyMatrix[a][b]
+                tDataEdges += tupleAdjacencyMatrix[a][b]
+            trainingDataMat[tMaxRow][10] = a
+            tMaxRow += 1
+    biggestProb = 0
     biggestProbCol = 0
     biggestProbVis = [0 for x in xrange(maxRow)]
     for a in xrange(maxRow):
         for b in xrange(binAmt):
-            denominator = intMatrix[a][10] * tDataEdges
             numerator = testDataMat[a][b] * colSum[b]
-            fldivision = float(numerator)/float(denominator)
-            if (fldivision > biggestProb):
-                biggestProb = fldivision
+            if (numerator > biggestProb):
+                biggestProb = numerator
                 biggestProbCol = b + 1
         biggestProbVis[a] = biggestProbCol
-        biggestProb = 0.0
-    return biggestProbVis, maxRow, testDataMat
+        biggestProb = 0
+    biggestTrainProb = 0
+    biggestProbTrainingVis = [0 for x in xrange(tMaxRow)]
+    for a in xrange(tMaxRow):
+        for b in xrange(binAmt):
+            numerator = trainingDataMat[a][b] * colSum[b]
+            if (numerator > biggestTrainProb):
+                biggestTrainProb = numerator
+                biggestProbCol = b + 1
+        biggestProbTrainingVis[a] = biggestProbCol
+        biggestTrainProb = 0
+    return biggestProbVis, biggestProbTrainingVis, maxRow, totalAuthors - maxRow, testDataMat, trainingDataMat
+
+def implement_bootstrap_bayes():
+    copyOfDict = dictionary.copy()
+    colSum = [0 for x in xrange(binAmt)]
+    bootstrapTestMat = [[0 for x in xrange(binAmt + 1)] for y in xrange(totalAuthors)]
+    bootstrapTrainingMat = [[0 for x in xrange(binAmt + 1)] for y in xrange(totalAuthors)]
+    for a in xrange(totalAuthors):
+        randomVal = random.uniform(0, totalAuthors)
+        rowVal = int(randomVal)
+        for b in xrange(binAmt):
+            bootstrapTrainingMat[a][b] = tupleAdjacencyMatrix[rowVal][b]
+            colSum[b] += tupleAdjacencyMatrix[rowVal][b]
+        bootstrapTrainingMat[a][10] = rowVal
+        copyOfDict[rowVal] = -1
+    tDataRows = 0
+    for a in xrange(totalAuthors - 1):
+        if(copyOfDict[a] >= 0):
+            for b in xrange(binAmt):
+                bootstrapTestMat[tDataRows][b] = tupleAdjacencyMatrix[a][b]
+                bootstrapTestMat[tDataRows][10] = a
+            tDataRows += 1
+    biggestProb = 0
+    biggestProbCol = 0
+    biggestProbVisTest = [0 for x in xrange(tDataRows)]
+    for a in xrange(tDataRows):
+        for b in xrange(binAmt):
+            numerator = bootstrapTestMat[a][b] * colSum[b]
+            if (numerator > biggestProb):
+                biggestProb = numerator
+                biggestProbCol = b + 1
+        biggestProbVisTest[a] = biggestProbCol
+        biggestProb = 0
+    biggestProbVisTraining = [0 for x in xrange(totalAuthors)]
+    for a in xrange(totalAuthors):
+        for b in xrange(binAmt):
+            numerator = bootstrapTrainingMat[a][b] * colSum[b]
+            if (numerator > biggestProb):
+                biggestProb = numerator
+                biggestProbCol = b + 1
+        biggestProbVisTraining[a] = biggestProbCol
+        biggestProb = 0
+    return biggestProbVisTest, biggestProbVisTraining, tDataRows, bootstrapTestMat, bootstrapTrainingMat
+
+
+
     # IMPLEMENT BAYES
 
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -93,28 +151,42 @@ def implement_bayes(coeff):
 evalColMatrix = [0 for x in xrange(binAmt)]
 for a in xrange(totalAuthors):
     for b in xrange(binAmt):
-        evalColMatrix[b] += intMatrix[a][b]
+        evalColMatrix[b] += tupleAdjacencyMatrix[a][b]
 evalBiggestProb = 0.0
 evalBiggestProbCol = 0
 evalBiggestProbMat = [[0 for x in xrange(2)] for y in xrange(totalAuthors)]
 for a in xrange(totalAuthors):
     for b in xrange(binAmt):
-        evalNum = evalColMatrix[b] * intMatrix[a][b]
-        if(evalNum > evalBiggestProb):
+        evalNum = evalColMatrix[b] * tupleAdjacencyMatrix[a][b]
+        if evalNum > evalBiggestProb:
             evalBiggestProb = evalNum
             evalBiggestProbCol = b + 1
     evalBiggestProbMat[a][0] = evalBiggestProbCol
-    if a < 317079:
+    if(a < 317079):
         evalBiggestProbMat[a][1] = dictionary[a]
     evalBiggestProb = 0.0
 
-def eval(mRow, testProbMatrix, TDMat):
+
+def checkAccuracy(testProbMatrix, mRow, TDMat):
     totalCorrect = 0
     for a in xrange(mRow):
         if testProbMatrix[a] == evalBiggestProbMat[TDMat[a][10]][0]:
             totalCorrect += 1
+    if(mRow == 0):
+        mRow += 1
     accuracy = float(totalCorrect)/float(mRow)
     return accuracy
+
+def checkTrainingAccuracy(trainingProbMatrix, totRow, trainMat):
+    totalCorrect = 0
+    for a in xrange(totRow):
+        if trainingProbMatrix[a] == evalBiggestProbMat[trainMat[a][10]][0]:
+            totalCorrect += 1
+    if(totRow == 0):
+        totRow += 1
+    accuracy = float(totalCorrect)/float(totRow)
+    return accuracy
+
 
 def checkHome():
     homeMat = [0 for x in xrange(binAmt)]
@@ -123,9 +195,9 @@ def checkHome():
     stayHomeLikelihoodMat = [0 for x in xrange(binAmt)]
     for a in xrange(totalAuthors):
         checker = evalBiggestProbMat[a][1] / binwidth
-        if (checker == 10):
+        if checker == 10:
             checker -= 1
-        if (checker == (evalBiggestProbMat[a][0] - 1)):
+        if checker == (evalBiggestProbMat[a][0] - 1):
             homeMat[checker] += 1
         totHighestProbInEachBin[evalBiggestProbMat[a][0] - 1] += 1
         qtyTuplesInEachBin[checker] += 1
@@ -149,30 +221,35 @@ timeTaken = 0.0
 kRuns = 10
 coeffMat = [0.01, 0.05, 0.1, 0.25, 0.4, 0.6, 0.75, 0.9, 0.95, 0.99]
 timingMat = [0 for x in xrange(10)]
+bootstrapAccuracy = 0.0
+bootstrapTrainingAccuracy = 0.0
 for a in xrange(kRuns):
-    numRuns = 5
-    accuracy = 0.0
+    numRuns = 3
+    holdoutAccuracy = 0.0
+    holdoutTrainingAccuracy = 0.0
     coefficient = coeffMat[a]
     start = timeit.default_timer()
     coeff = float(coefficient)
-    evalTime = 0.0
-    bayesTime = 0.0
     for b in xrange(numRuns):
-        biggestProbMat, maxRow, testDataMat = implement_bayes(coeff)
-        accuracy += eval(maxRow, biggestProbMat, testDataMat)
+        biggestProbMat, biggestProbTrainingMat, maxRow, tMaxRow, testDataMat, trainDataMat = implement_holdout_bayes(coeff)
+        bootstrapProbMat, bootstrapTrainMat, tDataRows, testMat, trainMat = implement_bootstrap_bayes()
+        bootstrapAccuracy += checkAccuracy(bootstrapProbMat, tDataRows, testMat)
+        bootstrapTrainingAccuracy += checkTrainingAccuracy(bootstrapTrainMat, totalAuthors, trainMat)
+        holdoutAccuracy += checkAccuracy(biggestProbMat, maxRow, testDataMat)
+        holdoutTrainingAccuracy += checkTrainingAccuracy(biggestProbTrainingMat, tMaxRow, trainDataMat)
     stop = timeit.default_timer()
     timeTaken += (stop - start)
     print "Run number:", a + 1
     print "--------------"
-    print "Ratio of training data to total data:        ", coeffMat[a]
-    print "Time for this run:                           ", stop - start
-    print "Number of test data tuples:                  ", int(math.ceil((1 - coeffMat[a]) * totalAuthors))
-    print "Correctly identified tuples", numRuns, "run avg%:      ",accuracy * (100.0/numRuns)
-    if(a < 9):
-        print "-------------------------------------------------------------------"
-    else:
-        print "-------------------------------------------------------------------------------"
+    print "Ratio of training data to total data:                        ", coeffMat[a]
+    print "Time for this run:                                           ", stop - start
+    print "Number of test data tuples:                                  ", int(math.ceil((1 - coeffMat[a]) * totalAuthors))
+    print "Correctly identified tuples in holdout", numRuns, "run avg%:           ",holdoutAccuracy * (100.0/numRuns)
+    print "Correctly identified train tuples in holdout", numRuns, "run avg%:     ", holdoutTrainingAccuracy * (100.0/numRuns)
+    print "-------------------------------------------------------------------------------"
 checkHome()
+print "Correctly identified tuples in bootstrap test data", numRuns * kRuns, "run avg%      ", bootstrapAccuracy * (100.0 / (numRuns * kRuns))
+print "Correctly identified tuples in bootstrap training data", numRuns * kRuns, "run avg%  ", bootstrapTrainingAccuracy * (100.0 / (numRuns * kRuns))
 print "Total run time:  ", timeTaken
 print "------------------------------"
 
